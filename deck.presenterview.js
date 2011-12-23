8fe6cfd9d92ce301e22622ea4a8045794424d62d
dix <dix@mediaman.de>
2011-12-23 12:29:02 +0100

    minor code changes, no text returned if no comment found

--- deck.presenterview.js
+++ deck.presenterview.js
@@ -1,28 +1,29 @@
 $(document).bind('deck.init', function() {
-    if (location.hash === "") {  
-        var href = window.location.href;
+    var href = window.location.href;
 
-        var hrefArr = href.split('/');
-        var hrefArrLength = hrefArr.length;
-        
-        if (hrefArr[hrefArrLength - 1] === '') {
-            delete hrefArr[hrefArrLength - 1];
-            delete hrefArr[hrefArrLength - 2];
-        }
-
-        var baseUrl = hrefArr.join('/');
-        var presenter = window.open(baseUrl + 'extensions/presenterview/deck.presenterview.html', 'My Window', 'width=' + screen.width + ', height=' + screen.height);
+    var hrefArr = href.split('/');
+    var hrefArrLength = hrefArr.length;
+    
+    if (hrefArr[hrefArrLength - 1] === '') {
+        delete hrefArr[hrefArrLength - 1];
+        delete hrefArr[hrefArrLength - 2];
     }
+
+    var baseUrl = hrefArr.join('/');
+
+    var presenter = window.open(baseUrl + 'extensions/presenterview/deck.presenterview.html', 'My Window', 'width=' + screen.width + ', height=' + screen.height);
 });
 
 $(document).bind('deck.change', function(event, from, to) {
-    curElem        = document.getElementById(location.hash.substring(1)).innerHTML;
-    startOfComment = curElem.indexOf('<!--');
-    endOfComment   = curElem.indexOf('-->');
+    curElem        = document.getElementById(location.hash.substring(1));
+    curElemHtml    = curElem.innerHTML;
+    startOfComment = curElemHtml.indexOf('<!--');
+    endOfComment   = curElemHtml.indexOf('-->');
+    
     if (startOfComment !== -1 && endOfComment !== -1) {
-        comment = curElem.substring(startOfComment + 4, endOfComment);
+        comment = curElemHtml.substring(startOfComment + 4, endOfComment);
     } else {
-        comment = 'No comment for this slide';
+        comment = '';
     }
     
     localStorage.setItem('slide_text', comment);
