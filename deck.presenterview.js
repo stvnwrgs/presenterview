ae31fbc34d0667aee7fcea64ff424d5b81174768
Steven Wirges <stevenwirges@googlemail.com>
2012-01-11 13:42:33 +0100

    stack

--- deck.presenterview.js
+++ deck.presenterview.js
@@ -1,44 +1,46 @@
-$(document).bind('deck.init', function() {
-    var href = window.location.href;
-    var hrefArr = href.split('/');
-    var hrefArrLength = hrefArr.length;
-    
-    if (hrefArr[hrefArrLength - 1] === '') {
-        delete hrefArr[hrefArrLength - 1];
-        delete hrefArr[hrefArrLength - 2];
-    }
+var isPresenterView = false;
 
-    var baseUrl = hrefArr.join('/');
-    var presenter = window.open(baseUrl + 'extensions/presenterview/deck.presenterview.html', 'My Window', 'width=' + screen.width + ', height=' + screen.height);
-    
-    // write css links to localstorage
-    if (document.styleSheets) {
-        var styleArr = [];
-        for (var i = 0; i < document.styleSheets.length - 1; i++) {
-            styleArr.push(document.styleSheets[i].href);
-        }
-        localStorage.setItem('stylesheets', JSON.stringify(styleArr));
-    }
+$(document).bind('deck.init', function() {
+	if (window.location.hash != "#no-presenterview") {
+		isPresenterView = true;
+		
+		var href = window.location.href;
+	    var hrefArr = href.split('/');
+	    var hrefArrLength = hrefArr.length;
+	    
+	    localStorage.setItem('presentation_url', hrefArr.join('/'));
+	    
+	    if (hrefArr[hrefArrLength - 1] === '') {
+	        delete hrefArr[hrefArrLength - 1];
+	        delete hrefArr[hrefArrLength - 2];
+	    }
+
+	    var baseUrl = hrefArr.join('/');
+	    
+	    var presenter = window.open(baseUrl + 'extensions/presenterview/deck.presenterview.html', 'My Window', 'width=' + screen.width + ', height=' + screen.height);	    
+	}  
 });
 
 $(document).bind('deck.change', function(event, from, to) {
-    if (document.getElementById(location.hash.substring(1)).nodeName === 'SECTION') {
-        curElem        = document.getElementById(location.hash.substring(1));
-        curElemHtml    = curElem.innerHTML;
-        startOfComment = curElemHtml.indexOf('<!--');
-        endOfComment   = curElemHtml.indexOf('-->');
-        
-        if (startOfComment !== -1 && endOfComment !== -1) {
-            comment = curElemHtml.substring(startOfComment + 4, endOfComment);
-        } else {
-            comment = '';
-        }
-        
-        localStorage.setItem('notes', comment);
-        if (curElem.nextElementSibling.nodeName === 'SECTION') {
-            localStorage.setItem('next_slide', curElem.nextElementSibling.innerHTML);
-        } else {
-            localStorage.setItem('next_slide', '');
-        }
-    }
+	if (!isPresenterView) {
+	    if (document.getElementById(location.hash.substring(1)).nodeName === 'SECTION') {
+	        curElem        = document.getElementById(location.hash.substring(1));
+	        curElemHtml    = curElem.innerHTML;
+	        startOfComment = curElemHtml.indexOf('<!--');
+	        endOfComment   = curElemHtml.indexOf('-->');
+	        
+	        if (startOfComment !== -1 && endOfComment !== -1) {
+	            comment = curElemHtml.substring(startOfComment + 4, endOfComment);
+	        } else {
+	            comment = '';
+	        }
+	        
+	        localStorage.setItem('notes', comment);
+	        if (curElem.nextElementSibling.nodeName === 'SECTION') {
+	            localStorage.setItem('next_slide_hash', curElem.nextElementSibling.id);
+	        } else {
+	            localStorage.setItem('next_slide_hash', '');
+	        }
+	    }
+	}
 });
